50d22e5bacad5885cc586df103bd1b19dfeae10b
cleverBe <cleverbernal123@gmail.com>
2024-08-25 21:37:31 -0400

    removed hasrouter

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,11 +1,11 @@
-import { createHashRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import { App } from "../App";
 import Default from "../screens/Default";
 import Product from "../screens/Product";
 import LoginForm from "../screens/Forms/LoginForm";
 import { LandingPage } from "../screens/LandingPage";
 
-const routes = createHashRouter(
+const routes = createBrowserRouter(
   [
     {
       path: "/",
